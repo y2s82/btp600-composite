5fdd8d485fca588edc8400418d86d8bf59c33790
y2s82 <y2s1982@gmail.com>
2019-01-24 08:19:00 -0500

    initial commit

---
+++ build/precache-manifest.752cd3478786ec0a1d8db891f64cc8bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "672e3be2eb5e974eb9fa",
+    "url": "/static/css/main.f2a7c088.chunk.css"
+  },
+  {
+    "revision": "672e3be2eb5e974eb9fa",
+    "url": "/static/js/main.672e3be2.chunk.js"
+  },
+  {
+    "revision": "8605ecfcfae0a7e04631",
+    "url": "/static/css/1.1657e28f.chunk.css"
+  },
+  {
+    "revision": "8605ecfcfae0a7e04631",
+    "url": "/static/js/1.8605ecfc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e2e936b38ecc5d1179fca3a0e7518885",
+    "url": "/static/media/worker.e2e936b3.png"
+  },
+  {
+    "revision": "d7f841f64135ae084784ae2a840cc8a4",
+    "url": "/static/media/diagram.d7f841f6.png"
+  },
+  {
+    "revision": "3b0d0b19a329f3ebeef5cea314750700",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,28 +1,139 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Impress, Step} from 'react-impressjs';
+import 'react-impressjs/styles/impress-demo.css';
 import './App.css';
+import workerpng from './worker.png';
+import diagrampng from './diagram.png';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Impress fallbackMessage={<p>Browser update required</p>}>
+
+            <Step id={'Title'} className={'step','slide'} data={{x:0,y:0,z:0,rotateX:90}}>
+            <h1>Composite</h1>
+            <h3>By Tony Sim</h3>
+            </Step>
+
+            <Step className={'step'} data={{x:0,y:-1300,z:0}}>
+            <h1>Worker</h1>
+            <Worker/>
+            </Step>
+
+            <Step className={'step'} data={{x:500,y:-1300,z:100}}>
+            <h1>A Team of Workers</h1>
+            <Team/>
+            </Step>
+
+            <Step className={'step'} data={{x:1200,y:-1300,z:700}}>
+            <h1>A Department of Teams of Workers</h1>
+            <Dept className={'dep'}/>
+            </Step>
+
+            <Step className={'step'} data={{x:0,y:-3000,z:0}}>
+            <h1>Worker</h1>
+            <Worker/>
+            </Step>
+
+            <Step className={'step'} data={{x:500,y:-3000,z:100}}>
+            <h1>A Team of Workers</h1>
+            <Team/>
+            <p>public:<br/>work()</p>
+            </Step>
+
+            <Step className={'step'} data={{x:1200,y:-3000,z:700}}>
+            <h1>A Department of Teams of Workers</h1>
+            <Dept className={'dep'}/>
+            <p>public:<br/>work()</p>
+            </Step>
+
+            <Step className={'step'} data={{x:2200,y:-3000,z:1400}}>
+            <div id={'compositebox'}> Composite </div>
+            <p>public:<br/>work()</p>
+            </Step>
+
+            <Step className={'step','slide'} data={{x:4000,y:-3000,z:0}}>
+            <img src={diagrampng} alt={'Diagram Missing'}/>
+            </Step>
+
+            <Step className={'step','slide'} data={{x:5000,y:-2500,z:0,rotateZ:20}}>
+            <h2>Consequences</h2>
+            <ul>
+            <li>+ Defines class hierarchies</li>
+            <li>+ Simplifies client code</li>
+            <li>+ Simplifies addition of new component</li>
+            <li>- Tougher to restrict components of a composite</li>
+            </ul>
+            </Step>
+
+            <Step className={'step','slide'} data={{x:6000,y:-2000,z:0,rotateZ:40}}>
+            <h2>Implementation p1</h2>
+            <ul>
+            <li>Explicit Parent Reference</li>
+            <li>Sharing Components</li>
+            <li>Maximizing the Component Interface</li>
+            <li>Declaring the Child Management Operations</li>
+            </ul>
+            </Step>
+
+            <Step className={'step','slide'} data={{x:7000,y:-1500,z:0,rotateZ:60}}>
+            <h2>Implementation p2</h2>
+            <ul>
+            <li>Avoid reference to Leaf/Composite in Component</li>
+            <li>Ordering Child</li>
+            <li>Caching for Performance</li>
+            <li>Deleting by composite, not leaf</li>
+            <li>Choosing Data Structure</li>
+            </ul>
+            </Step>
+
+
+            <Step className={'step','center'} data={{x:6000,y:-1000,z:0,scale:8}}>
+            <div>
+            <h1 className={'center'}>Question?</h1>
+            </div>
+            </Step>
+
+            </Impress>
+        );
+    }
+}
+
+
+class Worker extends Component {
+    render() {
+        return (
+            <div className={'worker'}>
+            <img src={workerpng} alt={"Worker"}/>
+            <p>public:<br/>work()</p>
+            </div>
+        );
+    }
+}
+class Team extends Component {
+    render() {
+        return (
+            <div className={'team'}>
+            <Worker className={'worker'}/>
+            <Worker className={'worker'}/>
+            <Worker className={'worker'}/>
+            </div>
+        );
+    }
+}
+
+class Dept extends Component {
+    render() {
+        return (
+            <div className={'dep'}>
+            <Team className={'team'}/>
+            <Team className={'team'}/>
+            <div className={'dephead'}>
+            <Worker className={'dephead'}/>
+            </div>
+            </div>
+        );
+    }
 }
 
 export default App;
