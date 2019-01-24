45e53078b086ecbc126abeb3d4608290c9d83f37
y2s82 <y2s1982@gmail.com>
2019-01-24 08:30:11 -0500

    Updates

---
+++ precache-manifest.2bf742193af685d95a60484e7b4c6c74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba3688e9ff1089d0bb9c",
+    "url": "/y2s82/btp600-composite/static/css/main.f2a7c088.chunk.css"
+  },
+  {
+    "revision": "ba3688e9ff1089d0bb9c",
+    "url": "/y2s82/btp600-composite/static/js/main.ba3688e9.chunk.js"
+  },
+  {
+    "revision": "8605ecfcfae0a7e04631",
+    "url": "/y2s82/btp600-composite/static/css/1.1657e28f.chunk.css"
+  },
+  {
+    "revision": "8605ecfcfae0a7e04631",
+    "url": "/y2s82/btp600-composite/static/js/1.8605ecfc.chunk.js"
+  },
+  {
+    "revision": "1057346ff6e276551c65",
+    "url": "/y2s82/btp600-composite/static/js/runtime~main.1057346f.js"
+  },
+  {
+    "revision": "e2e936b38ecc5d1179fca3a0e7518885",
+    "url": "/y2s82/btp600-composite/static/media/worker.e2e936b3.png"
+  },
+  {
+    "revision": "d7f841f64135ae084784ae2a840cc8a4",
+    "url": "/y2s82/btp600-composite/static/media/diagram.d7f841f6.png"
+  },
+  {
+    "revision": "41d8e56114eb912a5c2960461cc7be6f",
+    "url": "/y2s82/btp600-composite/index.html"
+  }
+];
